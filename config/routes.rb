3d7bfb936f3538f5e9c4a437f6460aa10a959664
AbhiEvolution <saini.abhishek7777@gmail.com>
2022-11-06 16:21:16 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # This line mounts Spree's routes at the root of your application.
+  # This means, any requests to URLs such as /products, will go to
+  # Spree::ProductsController.
+  # If you would like to change where this engine is mounted, simply change the
+  # :at option to something different.
+  #
+  # We ask that you don't use the :as option here, as Spree relies on it being
+
+  # Spree routes
+  mount Spree::Core::Engine, at: '/'
+
+  # sidekiq web UI
+  require 'sidekiq/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == Rails.application.secrets.sidekiq_username &&
+      password == Rails.application.secrets.sidekiq_password
+  end
+  mount Sidekiq::Web, at: '/sidekiq'
+end
